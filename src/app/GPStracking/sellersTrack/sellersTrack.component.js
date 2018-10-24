b11ff8cb30e818e6ca0df23c7cfd0ca0b09433c6
Micky <miguel.angel.angulo72@gmail.com>
2018-10-24 14:02:15 -0400

    Added support for multiple markers.

--- src/app/GPStracking/sellersTrack/sellersTrack.component.js
+++ src/app/GPStracking/sellersTrack/sellersTrack.component.js
@@ -11,55 +11,95 @@
   function sellersTrackController(SellersTrackEvents, SellersTrackService) {
     const vm = this;
     vm.sellersList = [];
-    var map;
-    var marker = null; // Create a marker array to hold your markers
+    vm.sellersSelectedList = [];
+    vm.selectAllSellersCheckbox = false;
+    vm.markers = [];
+
+    let map;
 
     vm.getSellers = getSellers;
+    vm.selectSeller = selectSeller;
+    vm.selectAllSellers = selectAllSellers;
 
     ////////////
 
     function initialize() {
-      vm.getSellers();
+      vm.getSellers().then(function(){
+        vm.selectAllSellers(true);
+      });
       initGoogleMaps();
       SellersTrackEvents.start();
-      SellersTrackEvents.addListeners({message: getCoordinates});
+      SellersTrackEvents.addListeners({changeLocation: listenCoordEvents});
     }
 
     function initGoogleMaps() {
       var mapOptions = {
-        zoom: 5,
-        center: new google.maps.LatLng(-15.8277324, -68.9899946),
+        zoom: 13.75,
+        center: new google.maps.LatLng(-16.5069882,-68.136291),
         scrollwheel: false,
         mapTypeId: 'roadmap'
-      }
+      };
       map = new google.maps.Map(document.getElementById('map'), mapOptions);
     }
 
-    function getCoordinates(msg) {
-      console.log('--->', JSON.parse(msg.data))
-      setMarkers(JSON.parse(msg.data))
+    function listenCoordEvents(msg) {
+      let data = JSON.parse(msg.data);
+      setMarkers(data);
     }
 
-    function setMarkers(coordinates) {
-      var beach = coordinates;
-      var myLatLng = new google.maps.LatLng(beach.latitude, beach.longitude);
-
-      if (marker != null) {
-        marker.setMap(null);
+    function setMarkers(event) {
+      let cacheMarkersList = [];
+      if(vm.markers.length > 0){
+        cacheMarkersList = _.filter(vm.markers, (marker) => {
+          return marker.userId !== event.userId && findSellerInCollection(marker.userId);
+        })
       }
+      if (findSellerInCollection(event.userId)) {
+        cacheMarkersList.push(newMarker(event));
+      }
+      _.forEach(vm.markers, (marker, index) => {
+        vm.markers[index].setMap(null)
+      });
+      vm.markers=[];
+      vm.markers = cacheMarkersList;
+    }
 
-      marker = new google.maps.Marker({
+    function findSellerInCollection (sellerId){
+      return _.find(vm.sellersSelectedList, seller => {
+        return seller.ci === sellerId
+      })
+    }
+
+    function newMarker(obj) {
+      let myLatLng = new google.maps.LatLng(obj.latitude, obj.longitude);
+      return new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: beach[0],
-        zIndex: 33.3
+        title: obj.title,
+        userId: obj.userId
       });
     }
 
     function getSellers() {
-      SellersTrackService.getSellers().then(response=>{
+      return SellersTrackService.getSellers().then(response=>{
         vm.sellersList = response.data.users
       })
+    }
+
+    function selectAllSellers(setValue) {
+      vm.selectAllSellersCheckbox = setValue || vm.selectAllSellersCheckbox;
+      _.forEach(vm.sellersList, function (seller) {
+        seller['active'] = vm.selectAllSellersCheckbox;
+      });
+      refreshSellersSelected();
+    }
+
+    function selectSeller() {
+      refreshSellersSelected();
+    }
+
+    function refreshSellersSelected() {
+      vm.sellersSelectedList = _.filter(vm.sellersList, {'active': true});
     }
 
     initialize();
