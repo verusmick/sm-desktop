e86235270bcfd138f2c78b4c8d0407d61d4cc3fa
Micky <miguel.angel.angulo72@gmail.com>
2018-11-06 16:49:19 -0400

    add api key maps

--- src/app/app.constants.js
+++ src/app/app.constants.js
@@ -3,5 +3,6 @@
 
   angular.module('app')
     .constant('SAMPLE_CONSTANT', 'Sample constant')
-    .constant('API_ENDPOINT', 'http://192.168.0.11:3007');
+    // .constant('API_ENDPOINT', 'http://192.168.0.11:3007');
+    .constant('API_ENDPOINT', 'http://104.248.223.66:3007');
 })();
