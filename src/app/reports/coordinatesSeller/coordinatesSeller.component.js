c9275718ca7d67811f979e9640c6318437f7da22
Micky <miguel.angel.angulo72@gmail.com>
2018-12-06 16:08:21 -0400

    Added first report.

--- src/app/reports/coordinatesSeller/coordinatesSeller.component.js
+++ src/app/reports/coordinatesSeller/coordinatesSeller.component.js
@@ -10,13 +10,35 @@
   /** @ngInject */
   function coordinatesSellerController(ReportsService) {
     const vm = this;
+    vm.sellerSelected = {};
+    vm.dateSince = '';
+    vm.dateUntil = '';
+    vm.sellersList = [];
+    vm.reportList = [];
 
+    vm.getSellers = getSellers;
+    vm.generateReport = generateReport;
+    vm.changeSeller = changeSeller;
 
     /////
     function initialize() {
-      ReportsService.getAllOrders().then(function(response){
-        console.log('init report coordinatesSeller', response)
+      vm.getSellers();
+    }
+
+    function getSellers() {
+      ReportsService.getSellers().then(function (response) {
+        vm.sellersList = response;
       });
+    }
+
+    function generateReport() {
+      ReportsService.getCoordinatesReport(vm.sellerSelected.ci, vm.dateSince, vm.dateUntil).then(response => {
+        vm.reportList = response;
+      });
+    }
+
+    function changeSeller() {
+      vm.reportList = [];
     }
 
     initialize();
--- src/app/reports/reports.service.js
+++ src/app/reports/reports.service.js
@@ -2,20 +2,70 @@
   'use strict';
 
   angular.module('app').factory('ReportsService', ReportsService);
+  angular.module('app').directive('exportToCsv', exportToCsv);
   function ReportsService($http, $q, API_ENDPOINT) {
     return {
-      getAllOrders: getAllOrders
+      getSellers: getSellers,
+      getCoordinatesReport: getCoordinatesReport
     };
 
-    function getAllOrders() {
+    function getSellers() {
       var deferred = $q.defer();
-      $http.get(API_ENDPOINT + '/orders')
+      $http.get(API_ENDPOINT + '/users?roleFilter=visitador')
         .then(function (response) {
-          deferred.resolve(response);
+          let list = response.data.data.users;
+          _.forEach(list, (seller, index) => {
+            list[index]['label'] = `${seller.firstName} ${seller.firstSurname}`
+          })
+          deferred.resolve(list);
         }, function (response) {
           deferred.reject(response);
         })
       return deferred.promise;
+    }
+
+    function getCoordinatesReport(userId, since, until) {
+      var deferred = $q.defer();
+      since = since.toISOString().split("T")[0];
+      until = until.toISOString().split("T")[0];
+      $http.get(API_ENDPOINT + '/history/sellers?since=' + since + '&until=' + until + '&userId=' + userId)
+        .then(function (response) {
+          deferred.resolve(response.data);
+        }, function (response) {
+          deferred.reject(response);
+        })
+      return deferred.promise;
+    }
+  }
+
+  function exportToCsv() {
+    return {
+      restrict: 'A',
+      ngTable: '=',
+
+      link: function (scope, element, attrs) {
+        var el = element[0];
+        element.bind('click', function (e) {
+          var table = angular.element(document.getElementById('#reportTable'))[0];
+          var csvString = '';
+          for (var i = 0; i < table.rows.length; i++) {
+            var rowData = table.rows[i].cells;
+            for (var j = 0; j < rowData.length; j++) {
+              csvString = csvString + rowData[j].innerHTML + ",";
+            }
+            csvString = csvString.substring(0, csvString.length - 1);
+            csvString = csvString + "\n";
+          }
+          csvString = csvString.substring(0, csvString.length - 1);
+          var a = $('<a/>', {
+            style: 'display:none',
+            href: 'data:application/octet-stream;base64,' + btoa(csvString),
+            download: 'emailStatistics.csv'
+          }).appendTo('body')
+          a[0].click()
+          a.remove();
+        });
+      }
     }
   }
 })();
