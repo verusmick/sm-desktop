1c3ddbe4fec74bfba61fa99e64c381be8ba2d851
Micky <miguel.angel.angulo72@gmail.com>
2018-12-13 10:20:24 -0400

    added validation in login.

--- src/app/login/login.component.js
+++ src/app/login/login.component.js
@@ -12,10 +12,16 @@
     const vm = this;
     vm.ci = '';
     vm.password = '';
+    vm.flagValidation = false;
 
+    vm.change = change;
     vm.login = login;
 
     ////
+
+    function change() {
+      vm.flagValidation = false
+    }
 
     function login() {
       var authUserData = {
@@ -24,9 +30,13 @@
       }
 
       LoginService.login(authUserData).then(function (response) {
-        $localStorage['usr'] = response.data.user;
-        $localStorage['tk'] = response.data.token;
-        $state.go('dashboard.sellersTrack');
+        if (response.status === 'error') {
+          vm.flagValidation = true
+        } else {
+          $localStorage['usr'] = response.data.user;
+          $localStorage['tk'] = response.data.token;
+          $state.go('dashboard.sellersTrack');
+        }
       }).catch(function () {
 
       });
