a55161076f587bccb814d489bee528a3898cd006
Micky <miguel.angel.angulo72@gmail.com>
2019-11-11 00:29:27 -0400

    Added show map in coordinate report.

--- src/app/reports/coordinatesSeller/coordinatesSeller.component.js
+++ src/app/reports/coordinatesSeller/coordinatesSeller.component.js
@@ -8,17 +8,19 @@
   });
 
   /** @ngInject */
-  function coordinatesSellerController(ReportsService) {
+  function coordinatesSellerController(ReportsService, ngDialog, $scope) {
     const vm = this;
     vm.sellerSelected = {};
     vm.dateSince = '';
     vm.dateUntil = '';
     vm.sellersList = [];
     vm.reportList = [];
+    let map;
 
     vm.getSellers = getSellers;
     vm.generateReport = generateReport;
     vm.changeSeller = changeSeller;
+    vm.openPositionModal = openPositionModal;
 
     /////
     function initialize() {
@@ -40,6 +42,55 @@
     function changeSeller() {
       vm.reportList = [];
     }
+
+    function newMarker(latitude, longitude) {
+      let myLatLng = new google.maps.LatLng(latitude, longitude);
+      return new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        label:{
+          color: 'black',
+          fontWeight: 'bold',
+        },
+        icon: {
+          labelOrigin: new google.maps.Point(11, 50),
+          url: '../../../assets/images/markers/marker_red.png',
+          size: new google.maps.Size(22, 40),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(11, 40),
+        }
+      });
+    }
+
+    function initGoogleMaps(latitude, longitude) {
+      console.log('init map')
+      var mapOptions = {
+        zoom: 18,
+        center: new google.maps.LatLng(latitude, longitude),
+        scrollwheel: true,
+        mapTypeId: 'roadmap'
+      };
+      map = new google.maps.Map(document.getElementById('mapModal'), mapOptions);
+    }
+    function  openPositionModal(latitude, longitude) {
+      let notExistCoordinatesWarningModal = ngDialog.open({
+        disableAnimation: true,
+        closeByDocument: true,
+        showClose: true,
+        template: 'app/reports/coordinatesSeller/showLocation.modal.html',
+        scope: $scope
+      });
+
+
+      setTimeout(function(){
+        initGoogleMaps(latitude, longitude);
+        newMarker(latitude, longitude)
+      },200)
+
+
+    }
+
+
 
     initialize();
   }
